baf7f795352b2b3e4eb6f8d7e4b41c8feab6f04b
Tomisin-O <onasanyatomisin@gmail.com>
2020-10-15 13:00:00 +0100

    recipe cards formatted

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, {useEffect, useState } from 'react';
 import './App.css';
-
+import Recipe from './Recipe';
 function App() {
   const APP_ID = "165be659";
   const APP_KEY = "66153f4a87d0c67abcf2a11be725d17c	";
 
+  const [recipes, setRecipes] = useState ([]);
   
   useEffect(() => {
     getRecipes ();
@@ -13,7 +14,7 @@ function App() {
   const getRecipes = async () => {
     const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json();
-    console.log(data);
+    setRecipes(data.hits);
     
   }
   return (
@@ -25,6 +26,14 @@ function App() {
           Search
         </button>
       </form>
+      {recipes.map(recipe =>(
+        <Recipe
+        key={recipe.recipe.label}
+        title={recipe.recipe.label}
+        calories={recipe.recipe.calories}
+        image={recipe.recipe.image}
+        />
+      ))}
     </div>
   );
 }
